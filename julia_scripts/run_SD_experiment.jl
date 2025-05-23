5cf7a1327946096ca3fd8289736b2318a2857cb4
Antonio <antonio.cruciani@gssi.it>
2025-05-23 23:52:45 +0300

    experiments SD non unif

--- julia_scripts/run_SD_experiment.jl
+++ julia_scripts/run_SD_experiment.jl
@@ -104,7 +104,7 @@ directed = false
 output = ""
 fractional = [2,4,5,10]
 
-#=
+
 datasets = ["01_musae_facebook_edges.txt","02_email_enron.txt","03_ca_astroph.txt"]
 
 #datasets = ["10_flickr.txt"]
@@ -540,7 +540,7 @@ end
 
 
 graphs_path = "../../percolation_centrality/components/"
-=#
+
 #graphs_path = "/home/antonio/Desktop/RES_PERCOLATION/EXACT/components/"
 #=
 datasets = ["01_musae_facebook_edges_lcc_in_50.txt","02_email_enron_lcc_in_50.txt","03_ca_astroph_lcc_in_50.txt"]
@@ -602,7 +602,7 @@ for eps in epsilon_list
     global j+=1
 end
 =#
-#=
+
 global  j=1
 
 datasets = ["15_cit_hepph_lcc_in_50.txt" ,"14_p2p_gnutella31_lcc_in_50.txt","11_soc_epinions_lcc_in_50.txt","12_soc_slashdot_lcc_in_50.txt","04_web_notredame_lcc_in_50.txt","06_web_google_lcc_in_50.txt"]
@@ -671,7 +671,7 @@ for denom in fractional
     end
     global j+=1
 end
-
+#=
 global  j=1 
 datasets = ["15_cit_hepph_lcc_in_50.txt" ,"14_p2p_gnutella31_lcc_in_50.txt","11_soc_epinions_lcc_in_50.txt","12_soc_slashdot_lcc_in_50.txt","04_web_notredame_lcc_in_50.txt","06_web_google_lcc_in_50.txt"]
 fractional = [2,4,5,10]
@@ -736,6 +736,7 @@ for denom in fractional
     global j+=1
 end
 =#
+graphs_path = "../../percolation_centrality/graphs/"
 
 @info("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
 @info("Running Experiments for Real-World Instances Experiment")
